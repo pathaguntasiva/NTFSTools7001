3335b38556f666fe151d4a60afaf1927aece2972
patha <patha@PATHAGUNTA_SIVA>
2025-05-29 08:36:19 +0530

    task 1 commited

--- GitProj01/src/main/java/nit/GitProj01/App.java
+++ GitProj01/src/main/java/nit/GitProj01/App.java
@@ -6,5 +6,6 @@ package nit.GitProj01;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Task 1 completed");
     }
 }
