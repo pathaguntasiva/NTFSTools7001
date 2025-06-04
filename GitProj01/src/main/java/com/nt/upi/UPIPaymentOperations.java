73adcf9b8e8e64803e3ca9532988b631bb237c09
patha <patha@PATHAGUNTA_SIVA>
2025-06-04 15:21:38 +0530

    TL-Reviewed second time

--- GitProj01/src/main/java/com/nt/upi/UPIPaymentOperations.java
+++ GitProj01/src/main/java/com/nt/upi/UPIPaymentOperations.java
@@ -2,7 +2,7 @@ package com.nt.upi;
 
 public class UPIPaymentOperations 
 {
-	//Performs the UPI Payment
+	//Performs the UPI Payment operations
 	public String doPayment(long PhNo,double amount)
 	{
 		return amount+" amount is paid to @phone number :: "+PhNo;
