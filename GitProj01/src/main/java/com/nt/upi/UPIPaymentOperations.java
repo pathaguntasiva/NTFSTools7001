b2af76f6e07e3f4238666450a1ba681d16ebc583
patha <patha@PATHAGUNTA_SIVA>
2025-06-04 15:09:47 +0530

    Dev1-Completed UPI Payment feature

---
+++ GitProj01/src/main/java/com/nt/upi/UPIPaymentOperations.java
@@ -0,0 +1,9 @@
+package com.nt.upi;
+
+public class UPIPaymentOperations 
+{
+	public String doPayment(long PhNo,double amount)
+	{
+		return amount+" amount is paid to phone number :: "+PhNo;
+	}
+}
