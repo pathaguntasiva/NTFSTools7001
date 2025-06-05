06519950038d4e304897f450b6c6073b81c4d549
patha <patha@PATHAGUNTA_SIVA>
2025-06-05 12:21:51 +0530

    TL Reviewed

--- GitProj01/src/main/java/com/nt/NB/NetBanking.java
+++ GitProj01/src/main/java/com/nt/NB/NetBanking.java
@@ -2,6 +2,7 @@ package com.nt.NB;
 
 public class NetBanking 
 {
+	//Net Banking Service
 	public String myNetBanking(long srcAccount,long destAccount,String IFSC,double amount)
 	{
 		return amount+" is transfering from "+srcAccount+" to "+destAccount;
