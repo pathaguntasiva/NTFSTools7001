d59d675bfa369b1bb7b22dd53fd69bedb2c55c1f
patha <patha@PATHAGUNTA_SIVA>
2025-06-12 13:51:28 +0530

    After improving upi payment

--- GitProj01/src/main/java/com/nt/upi/UPIPaymentOperations.java
+++ GitProj01/src/main/java/com/nt/upi/UPIPaymentOperations.java
@@ -3,7 +3,7 @@ package com.nt.upi;
 public class UPIPaymentOperations 
 {
 	//Performs the UPI Payment operations
-	public String doPayment(long PhNo,double amount)
+	public String doPayment(long PhNo,double amount,String upid)
 	{
 		return amount+" amount is paid to @phone number :: "+PhNo;
 	}
